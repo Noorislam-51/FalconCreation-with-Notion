f62891f05872af33b3aa0e15df55516dce578717
Noor <noorislam5114@gmail.com>
2025-06-10 10:36:53 +0530

    did object

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,7 +1,7 @@
 const portfolioData = [
 
   //thumbnail-----------------------------------------------------
- {
+  {
     id: 1,
     title: "7 Best Crypto Coin",
     type: "Graphic Design",
@@ -11,7 +11,7 @@ const portfolioData = [
     thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (1).png",
     freelancer: "Noor Islam"
   },
-  
+
   {
     id: 2,
     title: "Appointment Setting Scripts",
@@ -72,18 +72,9 @@ const portfolioData = [
     thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (4).jpg",
     freelancer: "Noor Islam"
   },
-  // {
-  //   id: 8,
-  //   title: "Handle Every Sales Objection",
-  //   type: "Graphic Design",
-  //   subtype: "YouTube Thumbnail Design",
-  //   mediaType: "image",
-  //   media: "/portfolio/graphic/Thumbnail/Thumbnail (4).jpg",
-  //   thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (4).jpg",
-  //   freelancer: "Noor Islam"
-  // },
+
   {
-    id: 9,
+    id: 8,
     title: "India's Win Confirm - Cricket World Cup",
     type: "Graphic Design",
     subtype: "YouTube Thumbnails",
@@ -93,7 +84,7 @@ const portfolioData = [
     freelancer: "Noor Islam"
   },
   {
-    id: 10,
+    id: 9,
     title: "Make Money From Shorts",
     type: "Graphic Design",
     subtype: "YouTube Thumbnails",
@@ -103,66 +94,158 @@ const portfolioData = [
     freelancer: "Noor Islam"
   },
   {
-  id: 11,
-  title: "Antidote - Lack of D3",
-  type: "Graphic Design",
-  subtype: "YouTube Thumbnails",
-  mediaType: "image",
-  media: "/portfolio/graphic/Thumbnail/Thumbnail (6).png",
-  thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (6).png",
-  freelancer: "Noor Islam"
+    id: 10,
+    title: "Antidote - Lack of D3",
+    type: "Graphic Design",
+    subtype: "YouTube Thumbnails",
+    mediaType: "image",
+    media: "/portfolio/graphic/Thumbnail/Thumbnail (6).png",
+    thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (6).png",
+    freelancer: "Noor Islam"
 
-},
-{
-  id: 12,
-  title: "Social Media Control",
-  type: "Graphic Design",
-  subtype: "YouTube Thumbnails",
-  mediaType: "image",
-  media: "/portfolio/graphic/Thumbnail/Thumbnail (6).jpg",
-  thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (6).jpg",
-  freelancer: "Noor Islam"
-},
-{
-  id: 13,
-  title: "Own The Market",
-  type: "Graphic Design",
-  subtype: "YouTube Thumbnails",
-  mediaType: "image",
-  media: "/portfolio/graphic/Thumbnail/Thumbnail (7).png",
-  thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (7).png",
-  freelancer: "Noor Islam"
-},
-{
-  id: 14,
-  title: "Secret of Success",
-  type: "Graphic Design",
-  subtype: "YouTube Thumbnails",
-  mediaType: "image",
-  media: "/portfolio/graphic/Thumbnail/Thumbnail (7).jpg",
-  thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (7).jpg",
-  freelancer: "Noor Islam"
-},
-{
-  id: 15,
-  title: "Indian Education System",
-  type: "Graphic Design",
-  subtype: "YouTube Thumbnails",
-  mediaType: "image",
-  media: "/portfolio/graphic/Thumbnail/Thumbnail (8).png",
-  thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (8).png",
-  freelancer: "Noor Islam"
-},
-{
-  id: 16,
-  title: "India vs China - Big Win",
-  type: "Graphic Design",
-  subtype: "YouTube Thumbnails",
-  mediaType: "image",
-  media: "/portfolio/graphic/Thumbnail/Thumbnail (9).png",
-  thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (9).png",
-  freelancer: "Noor Islam"
-},
+  },
+  {
+    id: 11,
+    title: "Social Media Control",
+    type: "Graphic Design",
+    subtype: "YouTube Thumbnails",
+    mediaType: "image",
+    media: "/portfolio/graphic/Thumbnail/Thumbnail (6).jpg",
+    thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (6).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 12,
+    title: "Own The Market",
+    type: "Graphic Design",
+    subtype: "YouTube Thumbnails",
+    mediaType: "image",
+    media: "/portfolio/graphic/Thumbnail/Thumbnail (7).png",
+    thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (7).png",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 13,
+    title: "Secret of Success",
+    type: "Graphic Design",
+    subtype: "YouTube Thumbnails",
+    mediaType: "image",
+    media: "/portfolio/graphic/Thumbnail/Thumbnail (7).jpg",
+    thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (7).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 14,
+    title: "Indian Education System",
+    type: "Graphic Design",
+    subtype: "YouTube Thumbnails",
+    mediaType: "image",
+    media: "/portfolio/graphic/Thumbnail/Thumbnail (8).png",
+    thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (8).png",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 15,
+    title: "India vs China - Big Win",
+    type: "Graphic Design",
+    subtype: "YouTube Thumbnails",
+    mediaType: "image",
+    media: "/portfolio/graphic/Thumbnail/Thumbnail (9).png",
+    thumbnail: "/portfolio/graphic/Thumbnail/Thumbnail (9).png",
+    freelancer: "Noor Islam"
+  },
+
+  {
+    id: 16,
+    title: "POHA - Indian Breakfast",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (1).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (1).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 17,
+    title: "UNMUTE YOUR WORLD - Audio Campaign",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (2).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (2).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 18,
+    title: "COOLZY - Underwear Product Banner",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (3).JPG",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (3).JPG",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 19,
+    title: "WOW Miss & Mrs Gujarat - Event Invitation",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (4).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (4).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 20,
+    title: "Big Discount Sale - 75% OFF",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (5).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (5).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 21,
+    title: "Sport Betting Platform Banner",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (6).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (6).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 22,
+    title: "Trending Movement - Dance Event",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (7).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (7).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 23,
+    title: "Samsung WAVY X - Transparent Smartphone",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (8).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (8).jpg",
+    freelancer: "Noor Islam"
+  },
+  {
+    id: 24,
+    title: "Nestle Scorched Almonds - Product Branding",
+    type: "Graphic Design",
+    subtype: "Posters & Banners",
+    mediaType: "image",
+    media: "/portfolio/graphic/Banner & Poster/Poster (1).jpg",
+    thumbnail: "/portfolio/graphic/Banner & Poster/Poster (1).jpg",
+    freelancer: "Noor Islam"
+  }
+
 
 
   //thumbnail-----------------------------------------------------
